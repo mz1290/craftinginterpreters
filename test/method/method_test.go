35f93560830f465a9ec87d9ead6e1b1eae3cf7d5
Michael Zimmerman <mzimme1290@gmail.com>
2022-07-15 14:04:50 -0500

    Add method unit tests

---
+++ test/method/method_test.go
@@ -0,0 +1,158 @@
+package method
+
+import (
+	"bufio"
+	"fmt"
+	"os/exec"
+	"testing"
+
+	"github.com/mz1290/craftinginterpreters/test/common"
+)
+
+var interpreter = ""
+
+func init() {
+	interpreter = common.GetInterpreter()
+	fmt.Printf("USING: %s\n", interpreter)
+}
+
+func TestArity(t *testing.T) {
+	file := "arity.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "no args\n1\n3\n6\n10\n15\n21\n28\n36\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestEmptyBlock(t *testing.T) {
+	file := "empty_block.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "nil\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestExtraArguments(t *testing.T) {
+	file := "extra_arguments.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 8] RuntimeError: expected 2 arguments but got 4`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
+
+func TestMissingArguments(t *testing.T) {
+	file := "missing_arguments.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 5] RuntimeError: expected 2 arguments but got 1`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
+
+func TestNotFound(t *testing.T) {
+	file := "not_found.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 3] RuntimeError: undefined variable property unknown`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
+
+func TestPrintBoundMethod(t *testing.T) {
+	file := "print_bound_method.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "<fn method>\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestReferToName(t *testing.T) {
+	file := "refer_to_name.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 3] RuntimeError: undefined variable method`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
+
+func TestToManyArguments(t *testing.T) {
+	file := "too_many_arguments.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 259] Error at "a": "can't have more than 255 arguments"`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
+
+func TestToManyParameters(t *testing.T) {
+	file := "too_many_parameters.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 258] Error at "a": "can't have more than 255 parameters"`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
