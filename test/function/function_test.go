5eefefded24d188ac400903de135ba869a49a017
Michael Zimmerman <mzimme1290@gmail.com>
2022-07-15 10:39:28 -0500

    Add function unit tests

---
+++ test/function/function_test.go
@@ -0,0 +1,219 @@
+package function
+
+import (
+	"bufio"
+	"fmt"
+	"os/exec"
+	"testing"
+
+	"github.com/mz1290/craftinginterpreters/test/common"
+)
+
+var interpreter = ""
+
+func init() {
+	interpreter = common.GetInterpreter()
+	fmt.Printf("USING: %s\n", interpreter)
+}
+
+func TestBodyMustBeBlock(t *testing.T) {
+	file := "body_must_be_block.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 3] Error at "123": "expected '{' before \"function\" body"`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
+
+func TestEmptyBody(t *testing.T) {
+	file := "empty_body.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "nil\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestExtraArguments(t *testing.T) {
+	file := "extra_arguments.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 6] RuntimeError: expected 2 arguments but got 4`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
+
+func TestLocalMutualRecursion(t *testing.T) {
+	file := "local_mutual_recursion.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 4] RuntimeError: undefined variable isOdd`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
+func TestLocalRecursion(t *testing.T) {
+	file := "local_recursion.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "21\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestMissingArguments(t *testing.T) {
+	file := "missing_arguments.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 3] RuntimeError: expected 2 arguments but got 1`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
+
+func TestMissingCommaInParameters(t *testing.T) {
+	file := "missing_comma_in_parameters.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 3] Error at "c": "expected ')' after parameters"`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
+
+func TestMutualRecursion(t *testing.T) {
+	file := "mutual_recursion.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "true\ntrue\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestNestedCallWithArguments(t *testing.T) {
+	file := "nested_call_with_arguments.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "hello world\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestParameters(t *testing.T) {
+	file := "parameters.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "0\n1\n3\n6\n10\n15\n21\n28\n36\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestPrint(t *testing.T) {
+	file := "print.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "<fn foo>\n<native fn>\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestRecursion(t *testing.T) {
+	file := "recursion.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "21\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestTooManyArguments(t *testing.T) {
+	file := "too_many_arguments.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 260] Error at "a": "can't have more than 255 arguments"`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
+
+func TestTooManyParameters(t *testing.T) {
+	file := "too_many_parameters.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 257] Error at "a": "can't have more than 255 parameters"`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
