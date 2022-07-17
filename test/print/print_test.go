add7989ba217597044bd38e41b2326d1fcccfed6
Michael Zimmerman <mzimme1290@gmail.com>
2022-07-17 08:41:21 -0500

    Add print test

---
+++ test/print/print_test.go
@@ -0,0 +1,35 @@
+package print
+
+import (
+	"bufio"
+	"fmt"
+	"os/exec"
+	"testing"
+
+	"github.com/mz1290/craftinginterpreters/test/common"
+)
+
+var interpreter = ""
+
+func init() {
+	interpreter = common.GetInterpreter()
+	fmt.Printf("USING: %s\n", interpreter)
+}
+
+func TestMissingArgument(t *testing.T) {
+	file := "missing_argument.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 2] Error at ";": "expected expression"`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
