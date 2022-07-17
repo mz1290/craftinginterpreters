0558bb69d875d291da43ca182f6a92263f0ace22
Michael Zimmerman <mzimme1290@gmail.com>
2022-07-17 16:48:59 -0500

    Add this unit tests

---
+++ test/this/this_test.go
@@ -0,0 +1,98 @@
+package this
+
+import (
+	//"bufio"
+	"bufio"
+	"fmt"
+	"os/exec"
+	"testing"
+
+	"github.com/mz1290/craftinginterpreters/test/common"
+)
+
+var interpreter = ""
+
+func init() {
+	interpreter = common.GetInterpreter()
+	fmt.Printf("USING: %s\n", interpreter)
+}
+
+func TestClosure(t *testing.T) {
+	file := "closure.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "Foo\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestNestedClass(t *testing.T) {
+	file := "nested_class.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "Outer instance\nOuter instance\nInner instance\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestNestedClosure(t *testing.T) {
+	file := "nested_closure.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "Foo\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestThisAtTopLevel(t *testing.T) {
+	file := "this_at_top_level.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 1] Error at "this": "can't use 'this' outside of a class"`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
+
+func TestThisInMethod(t *testing.T) {
+	file := "this_in_method.lox"
+	cmd := exec.Command(interpreter, file)
+	stdout, _ := cmd.Output()
+	expected := "baz\n"
+
+	if string(stdout) != expected {
+		t.Fatalf("expected %s got %s", expected, string(stdout))
+	}
+}
+
+func TestThisInTopLevelFunction(t *testing.T) {
+	file := "this_in_top_level_function.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 2] Error at "this": "can't use 'this' outside of a class"`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
