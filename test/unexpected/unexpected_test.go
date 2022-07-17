43839f4ab07e53f0591e2c30dcccbcd21302fe27
Michael Zimmerman <mzimme1290@gmail.com>
2022-07-17 17:43:41 -0500

    Add unexpected character unit test

---
+++ test/unexpected/unexpected_test.go
@@ -0,0 +1,35 @@
+package unexpected
+
+import (
+	"bufio"
+	"fmt"
+	"os/exec"
+	"testing"
+
+	"github.com/mz1290/craftinginterpreters/test/common"
+)
+
+var interpreter = ""
+
+func init() {
+	interpreter = common.GetInterpreter()
+	fmt.Printf("USING: %s\n", interpreter)
+}
+
+func TestUnexpectedCharacter(t *testing.T) {
+	file := "unexpected_character.lox"
+	cmd := exec.Command(interpreter, file)
+	stderr, _ := cmd.StderrPipe()
+	if err := cmd.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `[line 3] Error: "unexpected character"`
+
+	scanner := bufio.NewScanner(stderr)
+	scanner.Scan()
+	actualErr := scanner.Text()
+	if actualErr != expected {
+		t.Fatalf("expected error (%s) got %s", expected, actualErr)
+	}
+}
